dc51a23cc5755b02403a6678955afc85a9b04ad4
Sal Castoro <20549092+scastoro@users.noreply.github.com>
2022-04-05 10:34:51 -0400

    Add font to global styles Add Nav styles
    Import font into global styles and set html font-size. Add styling to
    navbar and nav child elements.

--- src/components/styles/Nav.styled.ts
+++ src/components/styles/Nav.styled.ts
@@ -3,15 +3,23 @@ import styled from 'styled-components';
 export const Nav = styled.nav`
   display: flex;
   justify-content: center;
+  padding: 2rem 0;
+  font-size: 2.4rem;
+  background-color: #2c6ebe;
+  color: #fcfbfa;
 
   ul {
     display: flex;
     list-style: none;
   }
   li {
-    margin-left: 1rem;
+    margin-right: 5.4rem;
+  }
+  li:last-child {
+    margin-right: 0;
   }
   a {
     text-decoration: none;
+    color: inherit;
   }
 `;
--- src/components/styles/global.ts
+++ src/components/styles/global.ts
@@ -1,8 +1,13 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@200;400;600;700;900&display=swap');
   *{
     margin: 0;
     padding: 0;
+  }
+  html {
+    font-size: 10px;
+    font-family: 'Inter', sans-serif;
   }
 `;
